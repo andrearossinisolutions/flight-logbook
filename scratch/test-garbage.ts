36986786798e8d5fa4a1a866122b5217323487b5
Andrea Rossini <andrea.rossini@bindingfuture.com>
2026-09-17 17:26:57 +0200

    Removed all unused scratches

--- scratch/test-garbage.ts
+++
@@ -1,8 +0,0 @@
-import { resolveQueryToIcaos } from "../lib/weather";
-
-async function test() {
-  const res = await resolveQueryToIcaos("DKSADMASP", "LIML");
-  console.log("resolveQueryToIcaos('DKSADMASP') ->", res);
-}
-
-test();
